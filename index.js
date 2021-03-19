a1538939e73411159e789d46dddc5b2c55936b40
Michael <Mlcorrieri@gmail.com>
2021-03-19 19:02:22 -0400

    Dom contect load done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code goes here
+// // Your code goes here
+// document.addEventListener("DOMContentLoaded", function () {
+//   console.log("The DOM has loaded");
+// });
+// console.log(
+//   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+// );
+// document.addEventListener("DOMContentLoaded", function () {
+//   let text = document.getElementById("text");
+//   text.innerHTML = "This is really cool!";
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+
+document.addEventListener("DOMContentLoaded", function () {
+  let text = document.getElementById("text");
+  text.innerHTML = "This is really cool!";
+});
